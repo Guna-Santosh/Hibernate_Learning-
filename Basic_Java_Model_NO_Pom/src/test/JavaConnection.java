37ee2dfeabb69c83653eed0bdcdeafef48026f2e
santosh <gunasantosh4@gmail.com>
2024-03-09 16:28:14 +0530

    Basic changes

--- Basic_Java_Model_NO_Pom/src/test/JavaConnection.java
+++ Basic_Java_Model_NO_Pom/src/test/JavaConnection.java
@@ -3,7 +3,7 @@ package test;
 public class JavaConnection {
 
 	public static void main(String[] args) {
-System.out.println("JavaConnection.main()");
+		System.out.println("JavaConnection.main()");
 	}
 
 }
