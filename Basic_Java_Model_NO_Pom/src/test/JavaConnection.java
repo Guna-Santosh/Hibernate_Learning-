18bbf520bbce4cf6a04de8a1f8b2159d3c1f30ed
santosh <gunasantosh4@gmail.com>
2024-03-09 16:31:37 +0530

    Test for connect git

--- Basic_Java_Model_NO_Pom/src/test/JavaConnection.java
+++ Basic_Java_Model_NO_Pom/src/test/JavaConnection.java
@@ -4,7 +4,7 @@ public class JavaConnection {
 
 	public static void main(String[] args) {
 		System.out.println("JavaConnection.main()");
-		System.out.println("");
+		System.out.println("HiberPractice");
 	}
 
 }
