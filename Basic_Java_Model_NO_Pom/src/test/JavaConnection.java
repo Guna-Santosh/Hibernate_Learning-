1c23d830103a0ec9a9a17afac53f81aac1e2fdd3
santosh <gunasantosh4@gmail.com>
2024-03-09 16:34:12 +0530

    Orm Connection

--- Basic_Java_Model_NO_Pom/src/test/JavaConnection.java
+++ Basic_Java_Model_NO_Pom/src/test/JavaConnection.java
@@ -5,6 +5,7 @@ public class JavaConnection {
 	public static void main(String[] args) {
 		System.out.println("JavaConnection.main()");
 		System.out.println("HiberPractice");
+		System.out.println("ORM Practice");
 	}
 
 }
