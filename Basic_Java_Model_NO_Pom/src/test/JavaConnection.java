60606baa52a413d160a4e8833622534ed97b2e16
santosh <gunasantosh4@gmail.com>
2024-04-20 12:20:12 +0530

    hibernate coding wise commited

--- Basic_Java_Model_NO_Pom/src/test/JavaConnection.java
+++ Basic_Java_Model_NO_Pom/src/test/JavaConnection.java
@@ -4,7 +4,8 @@ public class JavaConnection {
 
 	public static void main(String[] args) {
 		System.out.println("JavaConnection.main()");
-		
+		String name ="santosh";
+		System.out.println(name);
 	}
 
 }
